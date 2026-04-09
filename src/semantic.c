49cf39d97416b8602e9d4e20881e932ad838a9fd
JVR-07 <machado.jvr07@gmail.com>
2026-04-08 20:33:09 -0700

    feat(semantic): add validation for logical operators

--- src/semantic.c
+++ src/semantic.c
@@ -23,7 +23,9 @@ void validate_node(ASTNode *node) {
     
     switch (node->type) {
         case NODE_IDENTIFIER: {
+
             int id = lookup_symbol(node->t.lexeme);
+
             if (id == -1) {
                 semantic_error(node->t.line, "Variable no declarada o fuera de ámbito:", node->t.lexeme);
                 node->eval_type = TKN_ERROR;
@@ -34,53 +36,91 @@ void validate_node(ASTNode *node) {
         }
 
         case NODE_LITERAL: {
+
             if (node->t.type == TKN_LIT_INT) node->eval_type = TKN_INT;
             else if (node->t.type == TKN_LIT_FLOAT) node->eval_type = TKN_FLOAT;
             else if (node->t.type == TKN_LIT_STRING) node->eval_type = TKN_STRING;
             else if (node->t.type == TKN_LIT_TRUE || node->t.type == TKN_LIT_FALSE) node->eval_type = TKN_BOOL;
+            
             break;
         }
 
         case NODE_UNARY_OP: {
+            
             tokenType operand_type = node->left ? node->left->eval_type : TKN_EOF;
-            if (operand_type != TKN_INT && operand_type != TKN_FLOAT && operand_type != TKN_ERROR) {
-                semantic_error(node->t.line, "Operación unaria iterativa ++/-- sobre tipo no numérico.", node->t.lexeme);
-                node->eval_type = TKN_ERROR;
-            } else {
-                node->eval_type = operand_type;
+
+            if (node->t.type == TKN_NOT) {
+                
+                if (operand_type != TKN_BOOL && operand_type != TKN_ERROR) {
+                    semantic_error(node->t.line, "Operación unaria lógica ! sbore tipo no booleano", node->t.lexeme);
+                    node->eval_type = TKN_ERROR;
+                } else {
+                    node->eval_type = TKN_BOOL;
+                }
             }
-            break;
+            else if (node->t.type == TKN_SELF_PLUS || node->t.type == TKN_SELF_MINUS) {
+
+                if (operand_type != TKN_INT && operand_type != TKN_FLOAT && operand_type != TKN_ERROR) {
+                    semantic_error(node->t.line, "Operación unaria iterativa ++/-- sobre tipo no numérico.", node->t.lexeme);
+                    node->eval_type = TKN_ERROR;
+                } else {
+                    node->eval_type = operand_type;
+                }
+                break;
+            }
         }
 
         case NODE_BINARY_OP: {
+
             tokenType leftType = node->left ? node->left->eval_type : TKN_EOF;
             tokenType rightType = node->right ? node->right->eval_type : TKN_EOF;
 
             // si los hijos fallaron
             if (leftType == TKN_ERROR || rightType == TKN_ERROR) {
+                
                 node->eval_type = TKN_ERROR;
                 break;
             }
 
-            if (node->t.type == TKN_PLUS || node->t.type == TKN_MINUS || node->t.type == TKN_MULT || node->t.type == TKN_DIV || node->t.type == TKN_POWER) {
+            if (node->t.type == TKN_AND || node->t.type == TKN_OR) {
+
+                if(leftType == TKN_BOOL && rightType == TKN_BOOL) {
+                    
+                    node->eval_type = TKN_BOOL;
+
+                } else {
+                    
+                    semantic_error(node->t.line, "Tipos incompatibles en operación lógica:", node->t.lexeme);
+                    node->eval_type = TKN_ERROR;
+                }
+            }
+            else if (node->t.type == TKN_PLUS || node->t.type == TKN_MINUS || node->t.type == TKN_MULT || node->t.type == TKN_DIV || node->t.type == TKN_POWER) {
+                
                 if (leftType == TKN_INT && rightType == TKN_INT) {
+                
                     node->eval_type = TKN_INT;
+                
                 } else if ((leftType == TKN_FLOAT || leftType == TKN_INT) && 
                            (rightType == TKN_FLOAT || rightType == TKN_INT)) {
+                
                     node->eval_type = TKN_FLOAT;
+                
                 } else if (node->t.type == TKN_PLUS && leftType == TKN_STRING && rightType == TKN_STRING) {
-                    // Soporte de concatenación
+                
                     node->eval_type = TKN_STRING; 
+                
                 } else {
+                    
                     semantic_error(node->t.line, "Tipos incompatibles en operación aritmética:", node->t.lexeme);
                     node->eval_type = TKN_ERROR;
                 }
             } else if (node->t.type == TKN_GREATER || node->t.type == TKN_LESS || 
                        node->t.type == TKN_GREATER_EQUAL || node->t.type == TKN_LESS_EQUAL || 
                        node->t.type == TKN_EQUAL || node->t.type == TKN_NOT_EQUAL) {
-                // Return bool de operadores relacionales
+            
                 node->eval_type = TKN_BOOL;
             }
+
             break;
         }
 
@@ -89,12 +129,15 @@ void validate_node(ASTNode *node) {
             tokenType expr_type = node->right ? node->right->eval_type : TKN_EOF;
             
             if (target_type == TKN_ERROR || expr_type == TKN_ERROR) {
+                
                 node->eval_type = TKN_ERROR;
                 break;
             }
 
             if (target_type != expr_type) {
+                
                 if (!(target_type == TKN_FLOAT && expr_type == TKN_INT)) {
+                    
                     semantic_error(node->t.line, "Tipos incompatibles en la asignación.", NULL);
                     node->eval_type = TKN_ERROR;
                 }
